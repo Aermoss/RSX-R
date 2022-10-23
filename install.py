e376a4c32c27ab20890cf1b4e0e286e634c257d8
Yusuf Rençber <86446300+Aermoss@users.noreply.github.com>
2022-10-23 23:34:24 +0300

    Update install.py

--- install.py
+++ install.py
@@ -5,6 +5,8 @@ sys.dont_write_bytecode = True
 def main(argv):
     os.system("python setup.py install --user")
     shutil.rmtree("rsxr.egg-info")
+    shutil.rmtree("build")
+    shutil.rmtree("dist")
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
